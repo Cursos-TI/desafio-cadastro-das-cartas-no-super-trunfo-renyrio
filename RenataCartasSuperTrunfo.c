e20b3a3e0d56344a5ff89546e5b38068ed31a899
renyrio <renatamlgomesnyc@gmail.com>
2024-09-12 20:25:01 -0300

    Update RenataCartasSuperTrunfo.c
    Adicionei printf e scanf para as variáveis da fase novato do desafio.

--- RenataCartasSuperTrunfo.c
+++ RenataCartasSuperTrunfo.c
@@ -5,9 +5,18 @@
 
 
 int main() {
-    char opcao;
+    char Estado;
+    char Código da carta;
+    char Nome da cidade [50];
+    int População;
+    float Área;
+    float PIB;
+    int Número de pontos turísticos;
+    float Densidade populacional;
+    float PIB per capta;
+    float Super poder;
 
-    printf("Digite uma letra de 'A' a 'H' para o Estado: ");
+    printf("Digite o Estado escolhendo uma letra de 'A' a 'H': ");
     scanf(" %c", &opcao);
 
     // Verifica se o caractere está entre 'A' e 'H'
@@ -24,7 +33,7 @@ int main() {
                 break;
             case 'D':
                 printf("Você escolheu a letra 'D'\n");
- break;
+                break;
             case 'E':
                 printf("Você escolheu a letra 'E'\n");
                 break;
@@ -41,7 +50,23 @@ int main() {
     } else {
         printf("Letra inválida! Por favor, insira uma letra entre 'A' e 'H'.\n");
     }
+        printf("Digite o Código da carta: \n");
+        scanf(" %s", &codigodacarta);
 
+        printf("Digite o Nome da cidade: \n");
+        scanf(" %s", &nomedacidade);
+
+        printf("Digite a População: \n");
+        scanf(" %.2f", &populacao);
+
+        printf("Digite a Área: \n");
+        scanf(" %.2f km2", & nome);
+
+        printf("Digite o PIB: \n");
+        scanf(" %.2f reais", & nome);
+
+        printf("Digite o Número de pontos turísticos: \n");
+        scanf(" %d", & nome);
     return 0;
 }
 
